be1fccc427475cbcaf904101416f41d0743e8db6
junedlanja <junedlanja@gmail.com>
2018-11-01 22:59:41 +0530

    Carousel changes

--- components/CareerCarousel/index.js
+++ components/CareerCarousel/index.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import style from './index.css';
 import Carousel from './Carousel.js';
-import CareerCard from './CareerCard.js'
+import CareerCard from './CareerCard.js';
 
 class CareerCarousel extends Component {
   render() {
     const {items} = this.props;
     return (
-      <div className={style.container}>
-        <div className={style.carouselTitle}>Explore Career Profiles</div>
-
-        <Carousel items={items} Container={CareerCard}></Carousel>
-      </div>
+        <Carousel items={items} render={CareerCard}></Carousel>
     )
   }
 }
--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,7 @@ export default class MyDocument extends Document {
           <link rel='stylesheet' href='/static/css/base.css' />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css" />
           <link href="https://fonts.googleapis.com/css?family=Karla" rel="stylesheet"/>
+          <link rel='stylesheet' href='/static/css/alice-carousel.css' />
           {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> */}
           {/* <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> */}
         </Head>
